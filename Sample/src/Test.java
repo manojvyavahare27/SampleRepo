5807e1e0f2bb11fb93546e7982721d0acf10b139
manoj.vyavahare <manoj.vyavahare@riomed.com>
2022-04-09 09:30:41 +0530

    Partially commit

---
+++ Sample/src/Test.java
@@ -0,0 +1,12 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BMW b = new BMW();
+		b.start();
+		b.stop();
+		
+	}
+
+}
---
+++ Sample/src/car.java
@@ -0,0 +1,13 @@
+
+public class car {
+
+	public void start()
+	{
+		System.out.println("Car start method...");
+	}
+	
+	public void stop()
+	{
+		System.out.println("BMW stop method");
+	}
+}
